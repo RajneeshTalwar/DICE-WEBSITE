f370ba50af8bb3f22284307901b6d8a9e497c74a
naisha-gurung <naisha.gurung03@gmail.com>
2025-07-07 09:53:43 +0530

    Deployed 1.0

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import App from "./App.jsx";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>
 );
+
+
