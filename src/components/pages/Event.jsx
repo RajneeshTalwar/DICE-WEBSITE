a82a874d588ed4e39e61cd6b67126fad0f1fe407
naisha-gurung <naisha.gurung03@gmail.com>
2025-07-28 14:31:16 +0530

    Upcoming events moved

--- src/components/pages/Event.jsx
+++ src/components/pages/Event.jsx
@@ -1,4 +1,3 @@
-
 import Navbar from "../Navbar";
 import Footer from "../Footer";
 import FAQSection from "../FAQSection";
@@ -10,64 +9,25 @@ export default function Event() {
   return (
     <div className="Events">
       <Navbar />
-      <div className="container mx-auto pt-40">
-        <h1 className="text-4xl font-semibold mb-12 sm:text-5xl text-center">
-          Events
-        </h1>
-        <div>
-          <EventSlider />
-        </div>
-        <div className="w-full flex flex-col md:flex-row items-center justify-center mb-28">
-          <div className="md:w-1/2 p-4">
-            <h1 className="text-3xl sm:text-4xl font-semibold mb-8">
-              Some glimpse of our past events
-            </h1>
-            <p className="text-gray-500 text-md sm:text-lg">
-              Embark on a visual journey through our past events, presented in a
-              captivating video. Each scene offers a glimpse into our history,
-              unfolding like a storybook. Experience our triumphs, setbacks, and
-              the unfolding of time through dynamic imagery and narration. Dive
-              into our narrative, where every frame reveals a piece of our
-              collective heritage. Showcasing vibrant collaboration among
-              students and faculty from diverse fields. Through projects,
-              competitions, and industry engagements, DICE fosters innovation
-              and cross-disciplinary learning, preparing students for dynamic
-              career paths aligned with the evolving demands of the industry.
-            </p>
-          </div>
-          <div className="md:w-1/2 flex items-center justify-center p-2">
-            <video
-              src={diceVid3}
-              className="rounded-full w-[320px] h-[520px] object-cover shadow-2xl"
-              autoPlay
-              muted
-              loop
-            ></video>
-          </div>
-        </div>
-      </div>
+      <div className="container mx-auto pt-40 py-8 p-4">
 
-      {/* Removed Ongoing Events */}
-
-      <div className="container mx-auto pt-10 py-8 p-4">
-       
         <h1 className="text-4xl font-semibold mb-12 sm:text-5xl text-center">
           Upcoming Events
         </h1>
 
 
-        <div className="bg-black text-white py-2 overflow-hidden">
-  <div className="relative w-full">
-    <div className="flex animate-marquee space-x-12">
-      <span className="text-lg font-semibold whitespace-nowrap">
-         Participation in IISF 2025 | Workshop on Arduino |  Workshop on python |  Workshop on Networking |
-      </span>
-      <span className="text-lg font-semibold whitespace-nowrap">
-        Participation in IISF 2025 | Workshop on Arduino |  Workshop on python |  Workshop on Networking | 
-      </span>
-    </div>
-  </div>
-</div>
+        <div className="bg-black text-white py-6 overflow-hidden">
+          <div className="relative w-full">
+            <div className="flex animate-marquee space-x-12">
+              <span className="text-lg font-semibold whitespace-nowrap">
+                Participation in IISF 2025 | Workshop on Arduino |  Workshop on python |  Workshop on Networking |
+              </span>
+              <span className="text-lg font-semibold whitespace-nowrap">
+                Participation in IISF 2025 | Workshop on Arduino |  Workshop on python |  Workshop on Networking |
+              </span>
+            </div>
+          </div>
+        </div>
 
         {events_upcoming.length === 0 ? (
           <p className="text-2xl pt-10 text-center">
@@ -107,6 +67,46 @@ export default function Event() {
           </div>
         )}
       </div>
+      <div className="container mx-auto pt-40">
+        <h1 className="text-4xl font-semibold mb-12 sm:text-5xl text-center">
+          Events
+        </h1>
+        <div>
+          <EventSlider />
+        </div>
+        <div className="w-full flex flex-col md:flex-row items-center justify-center mb-28">
+          <div className="md:w-1/2 p-4">
+            <h1 className="text-3xl sm:text-4xl font-semibold mb-8">
+              Some glimpse of our past events
+            </h1>
+            <p className="text-gray-500 text-md sm:text-lg">
+              Embark on a visual journey through our past events, presented in a
+              captivating video. Each scene offers a glimpse into our history,
+              unfolding like a storybook. Experience our triumphs, setbacks, and
+              the unfolding of time through dynamic imagery and narration. Dive
+              into our narrative, where every frame reveals a piece of our
+              collective heritage. Showcasing vibrant collaboration among
+              students and faculty from diverse fields. Through projects,
+              competitions, and industry engagements, DICE fosters innovation
+              and cross-disciplinary learning, preparing students for dynamic
+              career paths aligned with the evolving demands of the industry.
+            </p>
+          </div>
+          <div className="md:w-1/2 flex items-center justify-center p-2">
+            <video
+              src={diceVid3}
+              className="rounded-full w-[320px] h-[520px] object-cover shadow-2xl"
+              autoPlay
+              muted
+              loop
+            ></video>
+          </div>
+        </div>
+      </div>
+
+      {/* Removed Ongoing Events */}
+
+
 
       <FAQSection />
       <Footer />
